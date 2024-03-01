0c8db4e8b3ea75edd0dc159118cc5902b8bc5c75
a-nodi <yoonhyeok.lee@postech.ac.kr>
2024-03-01 01:24:00 -0800

    [Style] Enhanced code readability and commented

--- apps/webget.cc
+++ apps/webget.cc
@@ -7,18 +7,19 @@
 using namespace std;
 
 void get_URL(const string &host, const string &path) {
-    // Your code here.
-
-    // You will need to connect to the "http" service on
-    // the computer whose name is in the "host" string,
-    // then request the URL path given in the "path" string.
-
-    // Then you'll need to print out everything the server sends back,
-    // (not just one call to read() -- everything) until you reach
-    // the "eof" (end of file).
+    /* parameters:
+     *     const string &host: A string that indicates host part of URL
+     *     const string &path: A string that indicates path part of URL
+     *
+     * return:
+     *     void
+     * */
 
     // Create message with given format
-    const string request_message = "GET " + path + " HTTP/1.1\r\n" + "HOST: " + host + "\r\n" + "Connection: close\r\n" + "\r\n";
+    const string request_message = "GET " + path + " HTTP/1.1\r\n" +
+                                   "HOST: " + host + "\r\n" +
+                                   "Connection: close\r\n" +
+                                   "\r\n";
 
     // Initialize TCP socket
     TCPSocket socket;
