95109b8f5703a1110a4e11337a0cbe11faa780b3
a-nodi <yoonhyeok.lee@postech.ac.kr>
2024-03-22 01:57:31 -0700

    [Feat] Implemented tcp_receiver, wrapping_integers, passed check_lab2

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,19 +1,57 @@
 #include "tcp_receiver.hh"
 
-// Dummy implementation of a TCP receiver
-
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    uint64_t raw_absolute_seqno;
+    size_t absolute_seqno;
+
+    uint64_t stream_index;
+    uint64_t syn_packet_offset;
+    string payload;
+
+    if (seg.header().syn) {
+        has_syn = true;
+        isn = WrappingInt32{seg.header().seqno.raw_value()};
+    }
+
+    if (!has_syn)
+        return;
+
+    if (seg.header().fin)
+        has_fin = true;
+
+    stream_index = static_cast<uint64_t>(_reassembler.stream_out().bytes_written());
+    raw_absolute_seqno = unwrap(seg.header().seqno, isn, stream_index);
+    syn_packet_offset = static_cast<uint64_t>(!seg.header().syn);
+
+    absolute_seqno = static_cast<size_t>(raw_absolute_seqno - syn_packet_offset);
+    payload = seg.payload().copy();
+
+    _reassembler.push_substring(payload, absolute_seqno, seg.header().fin);
+
+    return;
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    uint64_t stream_index;
+    uint64_t syn_packet_offset;
+    uint64_t fin_packet_offset;
+    optional<WrappingInt32> ackno;
+    if (!has_syn)
+        return nullopt;
 
-size_t TCPReceiver::window_size() const { return {}; }
+    stream_index = static_cast<uint64_t>(_reassembler.stream_out().bytes_written());
+    syn_packet_offset = static_cast<uint32_t>(has_syn);
+    fin_packet_offset = static_cast<uint32_t>(has_fin && _reassembler.empty());
+
+    ackno =
+        static_cast<optional<WrappingInt32>>(WrappingInt32(isn + syn_packet_offset + stream_index + fin_packet_offset));
+
+    return ackno;
+}
+
+size_t TCPReceiver::window_size() const { return _reassembler.stream_out().remaining_capacity(); }
--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,12 +1,9 @@
 #include "wrapping_integers.hh"
 
-// Dummy implementation of a 32-bit wrapping integer
+#include <iostream>
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
@@ -14,8 +11,11 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    const uint64_t moduler_base = 1ul << 32;
+    uint64_t isn_raw_value = static_cast<uint64_t>(isn.raw_value());
+    uint32_t seqno = static_cast<uint32_t>((isn_raw_value + n) % moduler_base);
+
+    return WrappingInt32{seqno};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,10 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    WrappingInt32 recent_ackno = wrap(checkpoint, isn);
+    uint64_t offset = static_cast<uint64_t>(n - recent_ackno);
+    bool overflow_occured = static_cast<int64_t>(checkpoint + offset) < 0;
+    uint64_t overflow_offset = overflow_occured ? (1ul << 32) : 0;
+
+    return checkpoint + offset + overflow_offset;
 }
