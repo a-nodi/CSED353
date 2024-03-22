81e4e16f09f4c5e9deab10ba98ee49d0591e840b
a-nodi <yoonhyeok.lee@postech.ac.kr>
2024-03-22 02:25:35 -0700

    [Style] Refactored the codes, and wrote some comments

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -6,6 +6,16 @@
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
+    /*
+     * The function that handles an input segment and stores the payload into the reassembler
+     *
+     * parameters:
+     *     const TCPSegment& seg: The input segment
+     *
+     * return:
+     *     void
+     */
+
     uint64_t raw_absolute_seqno;
     size_t absolute_seqno;
 
@@ -13,41 +23,60 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     uint64_t syn_packet_offset;
     string payload;
 
+    // If received SYN packet, set the initial sequence number
     if (seg.header().syn) {
         has_syn = true;
         isn = WrappingInt32{seg.header().seqno.raw_value()};
     }
 
+    // If no SYN packet received, Ignore the segment
     if (!has_syn)
         return;
 
+    // If received FIN packet, set the has_fin flag
     if (seg.header().fin)
         has_fin = true;
 
+    // Calculate the absolute sequence number to store the payload in the correct position
     stream_index = static_cast<uint64_t>(_reassembler.stream_out().bytes_written());
     raw_absolute_seqno = unwrap(seg.header().seqno, isn, stream_index);
     syn_packet_offset = static_cast<uint64_t>(!seg.header().syn);
-
     absolute_seqno = static_cast<size_t>(raw_absolute_seqno - syn_packet_offset);
+
+    // Get the payload from the segment
     payload = seg.payload().copy();
 
+    // Push the payload into the reassembler
     _reassembler.push_substring(payload, absolute_seqno, seg.header().fin);
 
     return;
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
+    /*
+     * The function that calculates the ackno
+     *
+     * parameters:
+     *      void
+     *
+     * return:
+     *     optional<WrappingInt32>: The calculated ackno (nullopt if no SYN packet received)
+     */
     uint64_t stream_index;
     uint64_t syn_packet_offset;
     uint64_t fin_packet_offset;
     optional<WrappingInt32> ackno;
+
+    // If no SYN packet received, Early termination
     if (!has_syn)
         return nullopt;
 
+    // Calculate the components that consists the ackno
     stream_index = static_cast<uint64_t>(_reassembler.stream_out().bytes_written());
     syn_packet_offset = static_cast<uint32_t>(has_syn);
     fin_packet_offset = static_cast<uint32_t>(has_fin && _reassembler.empty());
 
+    // Calculate the ackno
     ackno =
         static_cast<optional<WrappingInt32>>(WrappingInt32(isn + syn_packet_offset + stream_index + fin_packet_offset));
 
--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -12,8 +12,8 @@ using namespace std;
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
     const uint64_t moduler_base = 1ul << 32;
-    uint64_t isn_raw_value = static_cast<uint64_t>(isn.raw_value());
-    uint32_t seqno = static_cast<uint32_t>((isn_raw_value + n) % moduler_base);
+    uint64_t isn_raw_value = static_cast<uint64_t>(isn.raw_value());  // Type conversion to match the type of n
+    uint32_t seqno = static_cast<uint32_t>((isn_raw_value + n) % moduler_base);  // Seqno to absolute_seqno operation
 
     return WrappingInt32{seqno};
 }
@@ -29,10 +29,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    WrappingInt32 recent_ackno = wrap(checkpoint, isn);
-    uint64_t offset = static_cast<uint64_t>(n - recent_ackno);
-    bool overflow_occured = static_cast<int64_t>(checkpoint + offset) < 0;
-    uint64_t overflow_offset = overflow_occured ? (1ul << 32) : 0;
+    WrappingInt32 recent_ackno = wrap(checkpoint, isn);  // Relative ackno of the absolute recent ackno
+    uint64_t offset =
+        static_cast<uint64_t>(n - recent_ackno);  // Difference between the relative recent ackno and the n
+    bool overflow_occured = static_cast<int64_t>(checkpoint + offset) <
+                            0;  // Check if the overflow occured(overflow can occur: uint64_t + uint64_t -> uint64_t)
+    uint64_t overflow_offset = overflow_occured ? (1ul << 32) : 0;  // Offset to resolve the overflow
 
+    // Calculate the absolute_seqno
     return checkpoint + offset + overflow_offset;
 }
