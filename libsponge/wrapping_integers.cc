63a70658bdb2ad03989a743f4bf734bbe6462dbc
a-nodi <yoonhyeok.lee@postech.ac.kr>
2024-03-22 06:24:58 -0700

    [Style] Deleted unimportant const variable and enhanced readability

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -11,9 +11,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    const uint64_t moduler_base = 1ul << 32;
     uint64_t isn_raw_value = static_cast<uint64_t>(isn.raw_value());  // Type conversion to match the type of n
-    uint32_t seqno = static_cast<uint32_t>((isn_raw_value + n) % moduler_base);  // Seqno to absolute_seqno operation
+    uint32_t seqno = static_cast<uint32_t>((isn_raw_value + n) % 0x100000000ull);  // Seqno to absolute_seqno operation
 
     return WrappingInt32{seqno};
 }
@@ -34,7 +33,7 @@ uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
         static_cast<uint64_t>(n - recent_ackno);  // Difference between the relative recent ackno and the n
     bool overflow_occured = static_cast<int64_t>(checkpoint + offset) <
                             0;  // Check if the overflow occured(overflow can occur: uint64_t + uint64_t -> uint64_t)
-    uint64_t overflow_offset = overflow_occured ? (1ul << 32) : 0;  // Offset to resolve the overflow
+    uint64_t overflow_offset = overflow_occured ? 0x100000000ull : 0;  // Offset to resolve the overflow
 
     // Calculate the absolute_seqno
     return checkpoint + offset + overflow_offset;
